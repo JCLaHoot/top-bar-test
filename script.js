4f28a4e841935e5cca4e08e6f3626716a8133f5b
Jonathan Lahue <jonathanlahue@gmail.com>
2018-10-20 17:27:24 -0400

    prototype v1

--- script.js
+++ script.js
@@ -11,31 +11,58 @@
     let currentScrollY = 0;
 
 
+    let newY;
+    let scrollingUp;
+    let below120;
+    let below60;
 
 
    window.addEventListener('scroll', (event) => {
-       let newY = window.scrollY;
-       let scrollingUp = newY < currentScrollY;
-       let below120 = newY > 120;
+       newY = window.scrollY;
+       scrollingUp = newY < currentScrollY;
+       below120 = newY > 120;
+       below60 = newY > 60;
+
+       if (!below60) {
+           topBar.classList.add('full');
+           topBar.classList.remove('compact')
+       }
 
        if (below120) {
            topBar.classList.add('past-120');
+           topBar.classList.remove('full')
+           // topBar.style.top = `${currentScrollY - 120}px`
+
+
        }
        else {
            topBar.classList.remove('past-120');
-           topBar.classList.remove('compact');
 
        }
 
+       if(scrollingUp && !below120) {
+           topBar.style.transition = null;
+       }
 
-       if(scrollingUp) {
-           console.log('scrolling up');
-           if(below120) {
-               topBar.classList.add('compact');
+
+       if(scrollingUp && below120) {
+           if(!topBar.classList.contains('compact')){
+               topBar.style.transition = null;
+               setTimeout(() => {
+                       topBar.style.transition = 'none';
+               }, 1000)
+               // topBar.addEventListener('transitionend', () => {
+               //     topBar.style.transition = 'none';
+               // }, {once: true});
            }
-       } else  {
-           topBar.classList.remove('compact');
+           topBar.classList.add('compact');
 
+           console.log('scrolling up');
+       } else  {
+           if (below120) {
+               topBar.style.transition = null;
+               topBar.classList.remove('compact');
+           }
        }
        currentScrollY = newY;
    });
